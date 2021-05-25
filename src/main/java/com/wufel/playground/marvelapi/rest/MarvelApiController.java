0a893265a2a5c9b20653bbb74117d74fe5c440ae
wufel <wufelixhk@gmail.com>
2021-05-25 10:18:21 +0100

    initial commit

---
+++ src/main/java/com/wufel/playground/marvelapi/rest/MarvelApiController.java
@@ -0,0 +1,12 @@
+package com.wufel.playground.marvelapi.config;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MarvelApiController {
+
+    @GetMapping
+    
+
+}
---
+++ src/test/java/com/wufel/playground/marvelapi/MarvelApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wufel.playground.marvelapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarvelApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
